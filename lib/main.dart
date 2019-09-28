7caae7c9326af68190c38958592529d7461184ed
Mots08 <mots.199540@gmail.com>
2019-09-28 13:46:59 -0500

    Fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,4 @@
-import 'package:be_planet/src/pages/login.dart';
+import 'package:be_planet/src/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
  
-void main() => runApp(BePlanet());
- 
-class BePlanet extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Be PLanne App',
-      debugShowCheckedModeBanner: false,
-      home: Login()
-    );
-  }
-}
+void main() => runApp(MyApp());
